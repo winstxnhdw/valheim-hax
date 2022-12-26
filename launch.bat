81a4702f6ddfd10ed3d3b19722db596516b1da5e
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-12-27 04:12:38 +0800

    chore: remove now unnecessary dir clean step

--- launch.bat
+++ launch.bat
@@ -1,8 +1,5 @@
 @echo off
 
-rmdir /s /q "valheim-hax/References/NuGet"
-cls
-
 dotnet build valheim-hax
 start /wait /b ./submodules/SharpMonoInjectorCore/bin/SharpMonoInjector.exe inject -p valheim -a bin/valheim-hax.dll -n Hax -c Loader -m Load
 
