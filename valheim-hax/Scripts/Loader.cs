8ec9edceec18c546881e8602ff5ecdcf1a759bce
winstxnhdw <winstxnhdw@gmail.com>
2022-12-27 00:01:05 +0800

    chore: add game references

--- valheim-hax/Scripts/Loader.cs
+++ valheim-hax/Scripts/Loader.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 
 namespace Hax;
-public class Loader : MonoBehaviour {
+public class Loader : MonoBehaviour
+{
     static GameObject HaxGameObject { get; } = new GameObject();
     public static GameObject HaxModules { get; } = new GameObject();
 
     static void AddHaxModules<T>() where T : Component => Loader.HaxModules.AddComponent<T>();
     static void AddHaxGameObject<T>() where T : Component => Loader.HaxGameObject.AddComponent<T>();
 
-    public static void Load() {
+    public static void Load()
+    {
         Loader.LoadHaxGameObjects();
         Loader.LoadHaxModules();
     }
 
-    static void LoadHaxGameObjects() {
+    static void LoadHaxGameObjects()
+    {
         DontDestroyOnLoad(Loader.HaxGameObject);
 
         AddHaxGameObject<Cheat>();
@@ -24,15 +27,14 @@ public class Loader : MonoBehaviour {
         AddHaxGameObject<HaxObjects>();
     }
 
-    static void LoadHaxModules() {
+    static void LoadHaxModules()
+    {
         DontDestroyOnLoad(Loader.HaxModules);
 
-        AddHaxModules<UseCheats>();
-        AddHaxModules<PaddleMod>();
-        AddHaxModules<PersonMod>();
     }
 
-    public static void Unload() {
+    public static void Unload()
+    {
         Destroy(Loader.HaxModules);
         Destroy(Loader.HaxGameObject);
     }
