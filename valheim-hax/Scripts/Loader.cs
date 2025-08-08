cd477831b41321eb87fbcc8679ee34ab64fe6d1d
winstxnhdw <winstxnhdw@gmail.com>
2025-08-08 20:55:17 +0100

    refactor: make classes sealed

--- valheim-hax/Scripts/Loader.cs
+++ valheim-hax/Scripts/Loader.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -7,7 +7,7 @@ using HarmonyLib;
 
 namespace Hax;
 
-public class Loader : MonoBehaviour {
+sealed class Loader : MonoBehaviour {
     const string HarmonyID = "winstxnhdw.valheim-hax";
     static GameObject HaxGameObjects { get; } = new GameObject();
     public static GameObject HaxModules { get; } = new GameObject();
--- valheim-hax/Scripts/Patches/TerminalPatch.cs
+++ valheim-hax/Scripts/Patches/TerminalPatch.cs
@@ -1,9 +1,7 @@
 using HarmonyLib;
 
-namespace Hax;
-
 [HarmonyPatch(typeof(Terminal), nameof(Terminal.TryRunCommand))]
-public static class TerminalPatch {
+sealed class TerminalPatch {
     static bool Prefix(ref bool skipAllowedCheck) {
         skipAllowedCheck = true;
         return true;
